23b90838e509ae631a5b75e7bf282c02a688572e
michael <michaelglenn88@yahoo.com>
2022-07-23 13:10:02 -0400

    commit tree 23982e520b1a35bda3ce144b1a5df5697f64fc4f

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    # attr_accessor :job :name
+    def name=(name)
+        @name = name
+      end
+      def name
+        @name
+      end
+      # getter method
 
+      def job=(job)
+        @job = job
+      end
+      def job
+        @job
+      end
+    
 end
